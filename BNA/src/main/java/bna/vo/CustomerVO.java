66ab1a51eb77844ab6296f98019e90c61034094b
Geondol <93896063+geondol@users.noreply.github.com>
2022-05-31 16:34:23 +0900

    Add files via upload

---
+++ BNA/src/main/java/bna/vo/CustomerVO.java
@@ -0,0 +1,58 @@
+package bna.VO;
+
+public class CustomerVO {
+	
+	private String id,name,passwd1,passwd2,mail,tele,address;
+	private int age;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPasswd1() {
+		return passwd1;
+	}
+	public void setPasswd1(String passwd1) {
+		this.passwd1 = passwd1;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getTele() {
+		return tele;
+	}
+	public void setTele(String tele) {
+		this.tele = tele;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getPasswd2() {
+		return passwd2;
+	}
+	public void setPasswd2(String passwd2) {
+		this.passwd2 = passwd2;
+	}
+	
+	
+}
