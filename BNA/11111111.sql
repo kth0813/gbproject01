7d8947b5c82dae57b3c28f381361cdc726a69749
kth0813 <105401603+kth0813@users.noreply.github.com>
2022-06-09 16:12:13 +0900

    Delete 11111111.sql

--- BNA/11111111.sql
+++
@@ -1,7 +0,0 @@
-select * from LODTBL;
-select * from LODRESTBL;
-select * from LODREVTBL;
-
- 
-select * from lodrestbl a join lodtbl b on a.lodnum=b.lodnum;
-select * from actrestbl a join acttbl b on a.actnum=b.actnum;
